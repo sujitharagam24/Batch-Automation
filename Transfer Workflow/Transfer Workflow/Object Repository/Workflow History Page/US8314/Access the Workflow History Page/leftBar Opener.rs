c2db662130b39dbd0287fee7018dd4366e5e09c8
bilguun.amarsaikhan <bilguun.amarsaikhan@arrayinfotech.com>
2021-09-10 21:34:16 +0000

    Added Access the Workflow History Page Test case

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Workflow History Page/US8314/Access the Workflow History Page/leftBar Opener.rs
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>leftBar Opener</name>
+   <tag></tag>
+   <elementGuidId>7cfaada0-becb-4eb4-ac04-6cb35e215d32</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>path</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>d</name>
+      <type>Main</type>
+      <value>M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;app&quot;)/div[@class=&quot;mud-layout mud-drawer-close-responsive-md-left mud-drawer-left-clipped-never&quot;]/header[@class=&quot;mud-appbar mud-appbar-fixed mud-elevation-25&quot;]/div[@class=&quot;mud-toolbar mud-toolbar-gutters mud-toolbar-appbar&quot;]/button[@class=&quot;mud-button-root mud-icon-button mud-icon-button-color-inherit mud-ripple mud-ripple-icon mud-icon-button-edge-start&quot;]/span[@class=&quot;mud-icon-button-label&quot;]/svg[@class=&quot;mud-icon-root mud-svg-icon mud-inherit-text mud-icon-size-medium&quot;]/path[2]</value>
+   </webElementProperties>
+</WebElementEntity>
