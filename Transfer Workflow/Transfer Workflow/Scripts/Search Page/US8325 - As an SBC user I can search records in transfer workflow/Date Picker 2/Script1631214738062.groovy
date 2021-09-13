2b4870082eef4358f0eee6fe0d771df770b770bf
Soyoung Jung <soyoung.jung@arrayinfotech.com>
2021-09-13 22:36:40 +0000

    practice twf

--- Transfer Workflow/Transfer Workflow/Scripts/Search Page/US8325 - As an SBC user I can search records in transfer workflow/Date Picker 2/Script1631214738062.groovy
+++ Transfer Workflow/Transfer Workflow/Scripts/Search Page/US8325 - As an SBC user I can search records in transfer workflow/Date Picker 2/Script1631214738062.groovy
@@ -14,17 +14,45 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.utility.CommonMethods
+
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By as By
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import static org.junit.Assert.*
+import org.eclipse.core.runtime.Assert
 
-WebUI.navigateToUrl('https://devmodpimsapptransferworkflow.azurewebsites.net/search')
+WebUI.openBrowser(GlobalVariable.baseURL)
+WebUI.maximizeWindow()
+WebUI.click(findTestObject('Search Page Objectory/US8325/Dashboard Search Menu'))
+WebUI.click(findTestObject('Search Page Objectory/US8325/Date Picker/input_To_mud-input-slot mud-input-root mud-_a991a4'))
 
-WebUI.click(findTestObject('Object Repository/Search Page Objectory/US8325/Date Picker/Page_Transfer Workflow/input_To_mud-input-slot mud-input-root mud-_a991a4'))
+WebUI.clearText(findTestObject('Object Repository/Search Page Objectory/US8325/Page_Transfer Workflow/Recorded Date From'))
+WebUI.clearText(findTestObject('Object Repository/Search Page Objectory/US8325/Page_Transfer Workflow/Recorded Date To'))
 
-WebUI.setText(findTestObject('Object Repository/Search Page Objectory/US8325/Date Picker/Page_Transfer Workflow/input_To_mud-input-slot mud-input-root mud-_a991a4'), 
+WebUI.setText(findTestObject('Search Page Objectory/US8325/Date Picker/input_To_mud-input-slot mud-input-root mud-_a991a4'), 
     eventDateFrom1)
 
-WebUI.click(findTestObject('Object Repository/Search Page Objectory/US8325/Date Picker/Page_Transfer Workflow/span_Search'))
+WebUI.click(findTestObject('Search Page Objectory/US8325/span_Search'))
 
+
+WebDriver driver = DriverFactory.getWebDriver();
+List<WebElement> tableRows = driver.findElements(By.xpath("//table[@class='mud-table-root']/tbody/tr"));
+String[] row = new String[tableRows.size()];
+
+for(int i=0; i < tableRows.size(); i++) {
+	row[i] = tableRows.get(i).getText();
+}
+
+CustomKeywords.'com.utility.CommonMethods.print'(row)
+
+Assert.isNotNull(row)
+
+
+WebUI.closeBrowser()
