2e8cec33380c41e7c3deed88b92f20fdce55d24d
Michele Jazo <michele.jazo@arrayinfotech.com>
2021-09-17 10:39:58 -0700

    Clean up update adn delete merge

--- Transfer Workflow/Transfer Workflow/Scripts/Associated APN Panel/US8312 - As an SBC user I can delete an APN in Transfer Workflow/Delete Single APN/Script1631891903981.groovy
+++ Transfer Workflow/Transfer Workflow/Scripts/Associated APN Panel/US8312 - As an SBC user I can delete an APN in Transfer Workflow/Delete Single APN/Script1631891903981.groovy
@@ -16,37 +16,29 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import org.openqa.selenium.WebDriver as WebDriver
-import org.openqa.selenium.By as By
-import org.openqa.selenium.WebElement as WebElement
-import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
-import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
-import static org.junit.Assert.*
-import org.eclipse.core.runtime.Assert as Assert
-import com.kms.katalon.core.testobject.TestObject as TestObject
-
 
 /**
  *
  * Access the Worksheet Page through My Work tab
- * Verify Associated APN panel exists and delete single APN 
+ * Verify Associated APN panel exists and cancel deleting a single APN
  * Test case is dependent on Add Single APN script to be completed successfully
  * @author michele.jazo
  *
  */
 
-WebUI.callTestCase(findTestCase('Workflow History Page/US8314 - As an SBC user I can complete a document in Transfer workflow/Access the Workflow History Page'), 
-    [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Workflow History Page/US8314 - As an SBC user I can complete a document in Transfer workflow/Access the Workflow History Page'),
+	[:], FailureHandling.STOP_ON_FAILURE)
 
-//Find Associated APN panel
-WebUI.click(findTestObject('APN Panel/strong_Associated APNs'))
+WebUI.click(findTestObject('Object Repository/APN Panel/div_Associated APNs'))
 
 WebUI.click(findTestObject('Object Repository/APN Panel/DeleteTrashIcon'))
 
-WebUI.click(findTestObject('Object Repository/APN Panel/span_Delete'))
+WebUI.click(findTestObject('Object Repository/APN Panel/span_Delete_Cancel'))
 
-WebUI.click(findTestObject('Object Repository/APN Panel/div_Associated APN deleted'))
+WebUI.click(findTestObject('Object Repository/APN Panel/td_1234567891234'))
 
-WebUI.click(findTestObject('APN Panel/td_No Associated APNs'))
+findTestObject('Object Repository/APN Panel/UpdateIcon')
+
+findTestObject('Object Repository/APN Panel/DeleteTrashIcon')
 
 WebUI.closeBrowser()
