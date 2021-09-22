03bb1c945ba3632bac139a3c00ba96775b8e3449
Elian <elian.blanco@arrayinfotech.com>
2021-09-22 22:32:24 +0000

    changes pulled from origin

---
+++ CAMA/Appraisal Mass Appraisal(Cama)/Object Repository/01 Residential Property Appraisal/03 Appraisal Wizard Page Elements/02 Select Valuation Tab/Single Family Residential.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Single Family Residential</name>
+   <tag></tag>
+   <elementGuidId>6700883e-706f-4dd3-ac15-cc6d405e3bc0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//p[contains(text(), 'Single Family Residential')])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
