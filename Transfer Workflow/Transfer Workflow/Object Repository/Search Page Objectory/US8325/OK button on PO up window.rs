a0c81269efdf915e0e023f3988e9b27c26b5eb7c
bilguun.amarsaikhan <bilguun.amarsaikhan@arrayinfotech.com>
2021-10-12 18:16:22 +0000

    Error message when search without any input

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/OK button on PO up window.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OK button on PO up window</name>
+   <tag></tag>
+   <elementGuidId>7fbda0d4-bad8-4d2b-8c25-3f896013baff</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//*[text()='OK'])</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Pop up window MSG.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Pop up window MSG</name>
+   <tag></tag>
+   <elementGuidId>5dca3ae1-b94d-42ba-81af-3cee67508b30</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//div[@class='mud-dialog-content'])</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
