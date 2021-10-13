790583a736b7de0a463cad6dea706ffc0ebdfd0f
michele.jazo <michele.jazo@arrayinfotech.com>
2021-10-13 11:24:15 -0700

    Fix null value

--- Transfer Workflow/Transfer Workflow/Scripts/Worksheet Page/Associated APN Panel/US8310 - As an SBC user I can add single APN in Transfer Workflow/Access the Worksheet and APN Panel/Script1631728509454.groovy
+++ Transfer Workflow/Transfer Workflow/Scripts/Worksheet Page/Associated APN Panel/US8310 - As an SBC user I can add single APN in Transfer Workflow/Access the Worksheet and APN Panel/Script1631728509454.groovy
@@ -32,3 +32,4 @@ WebUI.callTestCase(findTestCase('Workflow History Page/US8314 - As an SBC user I
 WebUI.click(findTestObject('APN Panel/strong_Associated APN Panel'))
 
 WebUI.click(findTestObject('APN Panel/td_No Associated APNs'))
+
