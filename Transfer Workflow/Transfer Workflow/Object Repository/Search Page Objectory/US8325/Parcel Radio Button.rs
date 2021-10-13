08c3db5fef21b2bd9eaceb8a8b3dff1b17f3a65a
Menen Desta <menen.desta@arrayinfotech.com>
2021-10-13 15:54:17 +0000

    Search sort result by column Header

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Parcel Radio Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Parcel Radio Button</name>
+   <tag></tag>
+   <elementGuidId>3240f88a-a8d6-46c7-beaa-42844ff65574</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//*[@class='mud-radio-input'])[2]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Search Doc Type.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Search Doc Type</name>
+   <tag></tag>
+   <elementGuidId>92ae5722-f553-4e3f-9ac0-12e14a451ce3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@type='text'])[8]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
