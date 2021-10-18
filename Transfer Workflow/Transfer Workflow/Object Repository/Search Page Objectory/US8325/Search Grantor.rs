15ff6b95b8bf2f80faf3c4c83105035be4cfbe05
menen.desta <menen.desta@arrayinfotec.com>
2021-10-18 20:33:02 +0000

    Search by Grantor

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Search Grantor.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Search Grantor</name>
+   <tag></tag>
+   <elementGuidId>b439715e-f934-4001-af94-aebf85150708</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@type='text'])[6]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
