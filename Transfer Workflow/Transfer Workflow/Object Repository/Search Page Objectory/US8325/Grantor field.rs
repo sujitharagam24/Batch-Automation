49fca05d065a2a87b515b598f827e3657038eafd
bilguun.amarsaikhan <bilguun.amarsaikhan@arrayinfotech.com>
2021-10-21 15:51:26 +0000

    X button for individual fields on Search and hover over message on search results

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Grantor field.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Grantor field</name>
+   <tag></tag>
+   <elementGuidId>bb2a2685-f83a-43fb-bedb-f00e888a801b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//input[@type='text'])[6]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
