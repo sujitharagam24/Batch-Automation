202f9142734f62d1952fa35acb39163e65b3a4ee
michele.jazo <michele.jazo@arrayinfotech.com>
2021-10-25 07:11:05 -0700

    Update Add APN case

--- Transfer Workflow/Transfer Workflow/Scripts/Worksheet Page/Associated APN Panel/US8310 - As an SBC user I can add single APN in Transfer Workflow/Access the Worksheet and APN Panel/Script1631728509454.groovy
+++ Transfer Workflow/Transfer Workflow/Scripts/Worksheet Page/Associated APN Panel/US8310 - As an SBC user I can add single APN in Transfer Workflow/Access the Worksheet and APN Panel/Script1631728509454.groovy
@@ -31,3 +31,4 @@ WebUI.callTestCase(findTestCase('Workflow History Page/US8314 - As an SBC user I
 //Find Associated APN panel
 WebUI.click(findTestObject('APN Panel/strong_Associated APN Panel'))
 
+//div[@class='mud-select']
