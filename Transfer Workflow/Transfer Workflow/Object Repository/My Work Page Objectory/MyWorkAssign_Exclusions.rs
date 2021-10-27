589b478b65a0c335340f8bf44988a190bbc7a4f5
michele.jazo <michele.jazo@arrayinfotech.com>
2021-10-27 13:26:21 -0700

    My Work Auto Assign button

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/My Work Page Objectory/MyWorkAssign_Exclusions.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MyWorkAssign_Exclusions</name>
+   <tag></tag>
+   <elementGuidId>38bcda4d-c9e4-4208-ba59-a618f36a62e3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value> (//button[@aria-label='Auto Select'])[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Transfer Workflow/Transfer Workflow/Object Repository/My Work Page Objectory/MyWorkAssign_TimeShares.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MyWorkAssign_TimeShares</name>
+   <tag></tag>
+   <elementGuidId>257160ba-358e-4c2b-8cb6-5955ea3366d7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value> (//button[@aria-label='Auto Select'])[5]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
