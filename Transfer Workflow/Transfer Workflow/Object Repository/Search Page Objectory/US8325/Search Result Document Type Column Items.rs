f923b4a2fd75ad64ac3ef68beab6f16dda09357c
menen.desta <menen.desta@arrayinfotec.com>
2021-10-27 20:12:01 +0000

    Verify Document Type is sorted on Search Result Page

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Search Result Document Type Column Items.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Search Result Document Type Column Items</name>
+   <tag></tag>
+   <elementGuidId>a2d36bf1-a0a1-4895-8c49-40dce578f437</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//td[@class='mud-table-cell'])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
