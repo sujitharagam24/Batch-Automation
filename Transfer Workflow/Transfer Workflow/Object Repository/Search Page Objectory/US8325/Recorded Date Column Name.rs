8d7da7d8a5f76b6e6b9ae28ae16c37f0f6cfecb1
menen.desta <menen.desta@arrayinfotec.com>
2021-10-28 19:34:35 +0000

    Verify Recorded Date Column is Sorted on Search Result Page

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Recorded Date Column Name.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Recorded Date Column Name</name>
+   <tag></tag>
+   <elementGuidId>56fda253-8cae-42e9-bafb-929f7f44f3dc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//span[@class='mud-button-root mud-table-sort-label'])[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
