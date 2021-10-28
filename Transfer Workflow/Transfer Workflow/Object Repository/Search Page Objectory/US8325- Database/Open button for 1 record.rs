9a063305136b8c43a4f4ddcaeee688c4f38a76ef
bilguun.amarsaikhan <bilguun.amarsaikhan@arrayinfotech.com>
2021-10-28 15:03:56 -0400

    Database APN number match UI APN

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325- Database/Open button for 1 record.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Open button for 1 record</name>
+   <tag></tag>
+   <elementGuidId>6251b329-3332-4e8b-84d9-acc37598050b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//span[@class='mud-icon-button-label'])[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
