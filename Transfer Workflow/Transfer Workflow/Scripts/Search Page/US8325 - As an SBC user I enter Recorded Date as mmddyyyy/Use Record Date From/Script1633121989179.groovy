261afe7d4306d067c4d3c88e66b706e1790a0a63
michele.jazo <michele.jazo@arrayinfotech.com>
2021-10-28 09:52:43 -0700

    Update Search script

--- Transfer Workflow/Transfer Workflow/Scripts/Search Page/US8325 - As an SBC user I enter Recorded Date as mmddyyyy/Use Record Date From/Script1633121989179.groovy
+++ Transfer Workflow/Transfer Workflow/Scripts/Search Page/US8325 - As an SBC user I enter Recorded Date as mmddyyyy/Use Record Date From/Script1633121989179.groovy
@@ -32,18 +32,8 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
  *
  */
 
-<<<<<<< HEAD
-WebUI.openBrowser(GlobalVariable.baseURL, FailureHandling.STOP_ON_FAILURE)
-
-WebDriver driver = DriverFactory.getWebDriver()
-
-WebUI.maximizeWindow()
-
-WebUI.click(findTestObject('Object Repository/Search Page Objectory/US8325/Search tab'))
-=======
 WebUI.callTestCase(findTestCase('Search Page/US8325 - As an SBC user I can search records in transfer workflow/Access the Search Page'), 
     [:], FailureHandling.STOP_ON_FAILURE)
->>>>>>> 0ae875d8ff8af99bb4358cd08ebdcb83ef94cb8d
 
 WebUI.setText(findTestObject('Search Page Objectory/US8325/Record Date From Input'), "02022000")
 
