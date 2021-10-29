9959c757669af560a746edc4d46a965d84563606
menen.desta <menen.desta@arrayinfotec.com>
2021-10-29 19:13:21 +0000

    Verify Lead APN Column is Sorted on Search Result Page

---
+++ Transfer Workflow/Transfer Workflow/Object Repository/Search Page Objectory/US8325/Lead APN Column Name Items.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Lead APN Column Name Items</name>
+   <tag></tag>
+   <elementGuidId>91978f72-2735-4969-b588-e21a43757fa0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//td[@class='mud-table-cell'])[5]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
