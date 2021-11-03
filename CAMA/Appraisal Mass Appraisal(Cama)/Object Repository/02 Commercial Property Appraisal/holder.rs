0567309bcae0a9d45ffb0deb1d129093f1338c6c
Elian <elian.blanco@arrayinfotech.com>
2021-11-03 16:16:45 +0000

    Old project deleted and new Cama project renamed

--- CAMA/Appraisal Mass Appraisal(Cama)/Object Repository/02 Commercial Property Appraisal/holder.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>holder</name>
-   <tag></tag>
-   <elementGuidId>e2915bd3-5c8f-47bd-b6e0-8b2646296c68</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
