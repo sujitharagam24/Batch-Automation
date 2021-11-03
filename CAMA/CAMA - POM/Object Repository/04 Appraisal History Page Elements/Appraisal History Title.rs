4421e56c32a8e237d1ba3e92703d05d0e229f186
Elian <elian.blanco@arrayinfotech.com>
2021-11-03 02:43:10 +0000

    TC9320 modified (Assertion added)

---
+++ CAMA/CAMA - POM/Object Repository/04 Appraisal History Page Elements/Appraisal History Title.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Appraisal History Title</name>
+   <tag></tag>
+   <elementGuidId>7b2c8966-c803-4f95-9425-de2b30a742fc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h6[contains(text(), 'Appraisal History')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
