6aa59d7390086f7384416aca866f88d5f014383a
Elian <elian.blanco@arrayinfotech.com>
2021-11-03 04:58:16 +0000

    TC10261 added

---
+++ CAMA/CAMA - POM/Object Repository/03 Appraisal Wizard Page Elements/Breadcrumb Menu Option.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Breadcrumb Menu Option</name>
+   <tag></tag>
+   <elementGuidId>e13d0b1c-b8a7-4bd2-87d6-2298c63b5aa7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//ul[@class='mud-breadcrumbs mud-typography-body1']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ CAMA/CAMA - POM/Object Repository/03 Appraisal Wizard Page Elements/Breadcrumb separator.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Breadcrumb separator</name>
+   <tag></tag>
+   <elementGuidId>2d147afb-a69b-4410-8eb4-ec221ef679e8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@class='mud-breadcrumb-separator mud-ltr mud-flip-x-rtl']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
